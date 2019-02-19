52ad06bf80608f05d3c0011b45d2614db543c3ce
Leonardo Gatica <lgatica@protonmail.com>
2019-02-19 17:49:07 -0300

    refactor(script): migrate to async/await

--- src/script.js
+++ src/script.js
@@ -25,69 +25,70 @@ const isEnterprise = rut => {
 }
 
 module.exports = robot => {
-  robot.respond(/info-rut rut (.*)/i, res => {
-    const rut = res.match[1]
-    const fn = isEnterprise(rut)
-      ? infoRut.getEnterpriseByRut
-      : infoRut.getPersonByRut
-    fn(rut)
-      .then(result => {
-        if (!result) return res.reply('rut sin resultados')
-        res.send(`${result.name} (${result.rut})`)
-      })
-      .catch(err => {
-        res.reply('ocurrio un error al consultar el rut')
-        robot.emit('error', err, res)
-      })
+  robot.respond(/info-rut rut (.*)/i, async res => {
+    try {
+      const rut = res.match[1]
+      const fn = isEnterprise(rut)
+        ? infoRut.getEnterpriseByRut
+        : infoRut.getPersonByRut
+      const result = await fn(rut)
+      if (!result) return res.reply('rut sin resultados')
+      res.send(`${result.name} (${result.rut})`)
+    } catch (err) {
+      res.reply('ocurrio un error al consultar el rut')
+      robot.emit('error', err, res)
+    }
   })
 
-  robot.respond(/info-rut (persona|empresa) (.*)/i, res => {
-    const type = res.match[1]
-    const name = res.match[2]
-    const url = process.env.HUBOT_URL || 'http://localhost:8080'
-    const fn =
-      type === 'persona' ? infoRut.getPersonByName : infoRut.getEnterpriseByName
-    fn(name)
-      .then(results => {
-        if (results.length > 5) {
-          const query = querystring.stringify({ name, type })
-          const more = `\nMás resultados en ${url}/info-rut?${query}`
-          res.send(
-            results
-              .slice(0, 5)
-              .map(r => `${r.name} (${r.rut})`)
-              .join('\n')
-              .concat(more)
-          )
-        } else if (results.length > 0) {
-          res.send(results.map(r => `${r.name} (${r.rut})`).join('\n'))
-        } else {
-          res.reply(`no hay resultados para ${name}`)
-        }
-      })
-      .catch(err => {
-        res.reply('ocurrio un error al consultar el nombre')
-        robot.emit('error', err, res)
-      })
+  robot.respond(/info-rut (persona|empresa) (.*)/i, async res => {
+    try {
+      const type = res.match[1]
+      const name = res.match[2]
+      const url = process.env.HUBOT_URL || 'http://localhost:8080'
+      const fn =
+        type === 'persona'
+          ? infoRut.getPersonByName
+          : infoRut.getEnterpriseByName
+      const results = await fn(name)
+      if (results.length > 5) {
+        const query = querystring.stringify({ name, type })
+        const more = `\nMás resultados en ${url}/info-rut?${query}`
+        res.send(
+          results
+            .slice(0, 5)
+            .map(r => `${r.name} (${r.rut})`)
+            .join('\n')
+            .concat(more)
+        )
+      } else if (results.length > 0) {
+        res.send(results.map(r => `${r.name} (${r.rut})`).join('\n'))
+      } else {
+        res.reply(`no hay resultados para ${name}`)
+      }
+    } catch (err) {
+      res.reply('ocurrio un error al consultar el nombre')
+      robot.emit('error', err, res)
+    }
   })
 
-  robot.router.get('/info-rut', (req, res) => {
-    const type = req.query.type
-    const name = req.query.name
-    if (!type && !name) return res.send('faltan los parametros type y name')
-    const fn =
-      type === 'persona' ? infoRut.getPersonByName : infoRut.getEnterpriseByName
-    fn(name)
-      .then(results => {
-        if (results.length > 0) {
-          res.send(results.map(r => `${r.name} (${r.rut})`).join('<br/>'))
-        } else {
-          res.send(`no hay resultados para ${name}`)
-        }
-      })
-      .catch(err => {
-        res.send('Ocurrio un error al consultar el nombre')
-        robot.emit('error', err, res)
-      })
+  robot.router.get('/info-rut', async (req, res) => {
+    try {
+      const type = req.query.type
+      const name = req.query.name
+      if (!type && !name) return res.send('faltan los parametros type y name')
+      const fn =
+        type === 'persona'
+          ? infoRut.getPersonByName
+          : infoRut.getEnterpriseByName
+      const results = await fn(name)
+      if (results.length > 0) {
+        res.send(results.map(r => `${r.name} (${r.rut})`).join('<br/>'))
+      } else {
+        res.send(`no hay resultados para ${name}`)
+      }
+    } catch (err) {
+      res.send('Ocurrio un error al consultar el nombre')
+      robot.emit('error', err, res)
+    }
   })
 }
